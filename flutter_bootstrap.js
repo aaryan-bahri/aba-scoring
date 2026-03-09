ebcb30923988a7257822e2e4f4f7cb79cdadf781
cs-society-ashoka <cs.society@ashoka.edu.in>
2026-03-09 22:30:55 +0530

    Updated Setup Page. AI Assisted Design.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"72f2b18bb094f92f62a3113a8075240ebb59af
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3337014019"
+    serviceWorkerVersion: "781987193"
   }
 });
